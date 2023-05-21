2b11b54e139554f6cbdb940196f028d80e01c136
1100100 1100101 1100110 1110000 1110010 110000 1100111 <130287105+devpr0g@users.noreply.github.com>
2023-05-21 17:24:30 +0500

    Update ping_Pong.py

--- ping_Pong.py
+++ ping_Pong.py
@@ -1,6 +1,8 @@
+#ИМПОРТ МОДУЛЕЙ
 from pygame import *
 from random import randint
 
+#ФУНКЦИЯ ДЛЯ СОЗДАНИЯ БОТИКА
 def computer_move():
     if ball.rect.y > platform2.rect.y:
         platform2.rect.y += randint(2, 3)
@@ -8,7 +10,7 @@ def computer_move():
         platform2.rect.y -= randint(2, 3)
 
 
-
+#КЛАСС ДЛЯ СОЗДАНИЯ СПРАЙТА
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, w, h,):
         super().__init__()
@@ -19,7 +21,7 @@ class GameSprite(sprite.Sprite):
         self.rect.y = player_y
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
-
+#КЛАСС ДЛЯ ДВИЖЕНИЯ ИГРОКА
 class Player(GameSprite):
     def update_left(self):
         keys = key.get_pressed()
@@ -27,7 +29,7 @@ class Player(GameSprite):
             self.rect.y -= self.speed
         if keys[K_s] and self.rect.y < height - 80:
             self.rect.y += self.speed
-
+#ПЕРЕМЕННЫЕ
 back = (255, 228, 181)
 width = 600
 height = 500
@@ -38,26 +40,33 @@ game = True
 finish = False
 clock = time.Clock()
 fps = 60
-
+#СОЗДАНИЕ СПРАЙТОВ
 platform1 = Player('platform.png', 15, 200, 4, 150, 100)
 platform2 = Player('platform2.png', 455, 200, 4, 150, 100)
 ball = GameSprite('ball.png', 200, 200, 4, 100, 75) 
 
+#ШРИФТ
 font.init()
 font = font.Font(None, 35)
-
+#НАДПИСИ НА ИГРОВОМ ОКНЕ
 lose1 = font.render('PLAYER 1 LOSE!', True, (0, 0, 0))
 lose2 = font.render('PLAYER 2 LOSE!', True, (0, 0, 0))
 
+start_label = font.render('Press SPACE to start', True, (170, 0, 255))
+window.blit(start_label, (200,200))
 dx = 3
 dy = 3
-
+#ИГРОВОЙ ЦИКЛ
+start = False
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-    
-    if finish != True:
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                start = True
+
+    if finish != True and start == True:
         window.fill(back)
         platform1.update_left()
         computer_move()
